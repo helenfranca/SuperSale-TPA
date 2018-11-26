2b0e38775782f1c20344a9c3198e667a13fa35a1
unknown <you@example.com>
2018-11-26 19:27:56 -0200

    del

--- supersale.py
+++ supersale.py
@@ -1,29 +1,64 @@
-
-peso = [2,4,1,5]
-vet_preco = [14,23,56,34]
+vet_peso = [26, 22, 4, 18, 13, 9]
+vet_preco = [64, 85, 52, 99, 39, 54]
+vet_atual = []
 matriz_resultado = []
-vet_zero = []
 itens = 6
-capacidade = 3
+# capacidade + 1 por conta do zero inicial
+capacidade = 27
+degrau = 0
+vet_capac_pessoa = [23, 20, 20, 26]
 
 
-for i in range(0,capacidade):
-    vet_zero.append(0);
-matriz_resultado.append(vet_zero)
+def montaMatriz(capacidade, vet_peso, vet_atual, vet_preco, degrau, itens):
+    
+    for a in range(0, capacidade):
+        vet_atual.append(0)
+    matriz_resultado.append(vet_atual)
 
-for i in range(0,capacidade):
-    for j in range(1,itens):
-        a = matriz_resultado[j-1][i]
-        
-        if (peso[j] > i):
-            b = 0
-        else:
-            b = matriz_resultado[j-1][i-peso[j]] + vet_preco[j]
-            if (a > b):
-                matriz_resultado[j][i] = a
+    for k in sorted(vet_peso):
+        vet_atual = []
+        degrau += 1
+        for i in range(0, capacidade):
+            if (k <= i):
+                posPeso = vet_peso.index(k)
+                valorCorr = vet_preco[posPeso]
+                aux = i - k
+                valorSoma = valorCorr + matriz_resultado[degrau-1][aux]
+                if (valorSoma < matriz_resultado[degrau - 1][i]):
+                    vet_atual.append(matriz_resultado[degrau - 1][i])
+                else:
+                    vet_atual.append(valorSoma)
             else:
-                matriz_resultado[j][i] = b
+                if (matriz_resultado[degrau - 1][i] != 0):
+                    vet_atual.append(matriz_resultado[degrau - 1][i])
+                else:
+                    vet_atual.append(0)
+        matriz_resultado.append(vet_atual)
+    return matriz_resultado
 
 
 
-print(matriz_resultado)
+# imprime(matriz_resultado)
+
+# Faz a soma da família
+
+matriz_resultado =  montaMatriz(capacidade, vet_peso, vet_atual, vet_preco, degrau, itens)
+
+def calculaCapacidadeFamilia(matriz_resultado, vet_capac_pessoa):
+    linha = []
+    capacidade_familia = 0
+    linha = matriz_resultado[len(matriz_resultado)-1]
+    for capacidade in vet_capac_pessoa:
+        capacidade_familia += linha[capacidade]
+
+    return capacidade_familia
+
+def imprime(matriz_resultado):
+    linha = []
+    for i in range(0, capacidade):
+        print("", i, end=",")
+    print()
+    for linha in matriz_resultado:
+        print(linha)
+
+print(calculaCapacidadeFamilia(matriz_resultado, vet_capac_pessoa))
